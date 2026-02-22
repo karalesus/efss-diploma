72fdca7f3cc984ca21175c4ced0e7595d9b535a3
karalesus <karalesus@gmail.com>
2026-02-22 19:42:34 +0300

    init project

---
+++ src/main/java/ru/rutmiit/DistraApplication.java
@@ -0,0 +1,11 @@
+package ru.rutmiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistraApplication {
+    static void main(String[] args) {
+        SpringApplication.run(DistraApplication.class, args);
+    }
+}
